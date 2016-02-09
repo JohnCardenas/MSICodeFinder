3201fd44663284b32252383b50cb8073e8e0469f
JohnCardenas <Cardenas.John.J@gmail.com>
2016-02-09 14:37:59 -0800

    Initial commit

---
+++ MSICodeFinder/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSICodeFinder
+{
+    class Program
+    {
+        enum CodeFinderMode
+        {
+            Undefined = 0,
+            ProductCode,
+            UpgradeCode
+        };
+
+        private static CodeFinderMode FinderMode;
+
+        private static Guid Code;
+
+        static void PrintUsageMessage()
+        {
+            string programName = Path.GetFileName(Assembly.GetExecutingAssembly().Location);
+
+            Console.WriteLine("USAGE: " + programName + " mode guid");
+            Console.WriteLine("   mode = ProductCode | UpgradeCode");
+            Console.WriteLine("   guid = MSI GUID to find the ProductCode or UpgradeCode for");
+            Console.WriteLine("");
+            Console.WriteLine("When mode is set to ProductCode, installed product codes belonging to the specified upgrade code GUID are returned, one per line.");
+            Console.WriteLine("When mode is set to UpgradeCode, the upgrade code of the specified product code GUID is returned, if installed.");
+        }
+
+        static int Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                PrintUsageMessage();
+                return 1;
+            }
+
+            if (args[0].ToLower() == "productcode")
+                FinderMode = CodeFinderMode.ProductCode;
+            else if (args[0].ToLower() == "upgradecode")
+                FinderMode = CodeFinderMode.UpgradeCode;
+            else
+            {
+                Console.WriteLine("Invalid mode specified.\n");
+                PrintUsageMessage();
+                return 1;
+            }
+
+            if (!Guid.TryParse(args[1], out Code))
+            {
+                Console.WriteLine("Invalid MSI GUID specified.\n");
+                PrintUsageMessage();
+                return 1;
+            }
+
+            if (FinderMode == CodeFinderMode.ProductCode)
+            {
+                List<Guid> productCodes = RegistryHelper.GetProductCodes(Code);
+
+                foreach (Guid pCode in productCodes)
+                {
+                    Console.WriteLine(pCode.ToString("B"));
+                }
+            }
+            else
+            {
+                Guid? upgradeCode = RegistryHelper.GetUpgradeCode(Code);
+
+                if (upgradeCode != null)
+                    Console.WriteLine(((Guid)upgradeCode).ToString("B"));
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ MSICodeFinder/RegistryHelper.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace MSICodeFinder
+{
+    /**
+     * Some code from http://stackoverflow.com/questions/17936064/how-can-i-find-the-upgrade-code-for-an-installed-application-in-c
+     */
+    internal static class RegistryHelper
+    {
+        private const string UPGRADE_CODE_REGISTRY_KEY = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UpgradeCodes";
+
+        private static readonly int[] GuidRegistryFormatPattern = new[] { 8, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2 };
+
+        public static Guid? GetUpgradeCode(Guid productCode)
+        {
+            // Convert the product code to the format found in the registry
+            var productCodeSearchString = ConvertToRegistryFormat(productCode);
+
+            // Open the upgrade code registry key
+            using (RegistryKey upgradeCodeRegistryRoot = Registry.LocalMachine.OpenSubKey(UPGRADE_CODE_REGISTRY_KEY))
+            {
+                if (upgradeCodeRegistryRoot == null)
+                    return null;
+
+                // Iterate over each sub-key
+                foreach (string subKeyName in upgradeCodeRegistryRoot.GetSubKeyNames())
+                {
+                    using (RegistryKey subkey = upgradeCodeRegistryRoot.OpenSubKey(subKeyName))
+                    {
+                        if (subkey == null)
+                            continue;
+
+                        // Check for a value containing the product code
+                        if (subkey.GetValueNames().Any(s => s.IndexOf(productCodeSearchString, StringComparison.OrdinalIgnoreCase) >= 0))
+                        {
+                            // Extract the name of the subkey from the qualified name
+                            string formattedUpgradeCode = subkey.Name.Split('\\').LastOrDefault();
+
+                            // Convert it back to a Guid
+                            return ConvertFromRegistryFormat(formattedUpgradeCode);
+                        }
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        public static List<Guid> GetProductCodes(Guid upgradeCode)
+        {
+            List<Guid> codes = new List<Guid>();
+
+            // Convert the upgrade code to the format found in the registry
+            var upgradeCodeKeyName = ConvertToRegistryFormat(upgradeCode);
+
+            // Open the upgrade code registry key
+            using (RegistryKey upgradeCodeRegKey = Registry.LocalMachine.OpenSubKey(UPGRADE_CODE_REGISTRY_KEY + "\\" + upgradeCodeKeyName))
+            {
+                if (upgradeCodeRegKey == null)
+                    return codes;
+
+                // Iterate over each product code value
+                foreach (string valueName in upgradeCodeRegKey.GetValueNames())
+                {
+                    codes.Add(ConvertFromRegistryFormat(valueName));
+                }
+            }
+
+            return codes;
+        }
+
+        private static string ConvertToRegistryFormat(Guid productCode)
+        {
+            return Reverse(productCode, GuidRegistryFormatPattern);
+        }
+
+        private static Guid ConvertFromRegistryFormat(string upgradeCode)
+        {
+            if (upgradeCode == null || upgradeCode.Length != 32)
+                throw new FormatException("Product code was in an invalid format");
+
+            upgradeCode = Reverse(upgradeCode, GuidRegistryFormatPattern);
+
+            return Guid.Parse(upgradeCode);
+        }
+
+        private static string Reverse(object value, params int[] pattern)
+        {
+            // Strip the hyphens
+            var inputString = value.ToString().Replace("-", "");
+
+            var returnString = new StringBuilder();
+
+            var index = 0;
+
+            // Iterate over the reversal pattern
+            foreach (var length in pattern)
+            {
+                // Reverse the sub-string and append it
+                returnString.Append(inputString.Substring(index, length).Reverse().ToArray());
+
+                // Increment our posistion in the string
+                index += length;
+            }
+
+            return returnString.ToString();
+        }
+    }
+}
